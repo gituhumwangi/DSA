dcd712b89b9690b7ed667e9a0043f49bfa92eeec
dennis <dennis.mwangi@student.moringaschool.com>
2024-01-11 23:56:37 +0300

    Added file

--- linked_list.py
+++ linked_list.py
@@ -33,4 +33,52 @@ class Linkedlist:
             current = current.next_node
 
         return count
+    def add(self, data):
+        #Adds new node containing data at the head of the list
+        #Takes O(1) time
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
+
+
+    def search(self, key):
+        # Searches for the first node that matches the key 
+        # Returns the node or "None"
+        #Takes o(n) time
+
+        current = self.head
+
+        while current: 
+            if current.data == key:
+                return current
+            else:
+                current = current.next_node
+        return None
+     
+    def insert(self, data, index):
+        if index == 0:
+            self.add(data)
+        if index > 0:
+            new = Node(data)
+
+            position = index
+            current = self.head 
+        
+
+
+    def __repr__(self):
+        #Return a string representation of a list
+        #Takes O(n) time
+        nodes = []
+        current = self.head
+
+        while current:
+            if current is self.head:
+                nodes.append("[Head: %s]" % current.data)
+            elif current.next_node is None:
+                nodes.append("[Tail: %s]" % current.data)
+            else: 
+                nodes.append("[%s]" % current.data)
+            current = current.next_node
+        return '->' .join(nodes)
         
---
+++ linked_list_example2.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+
+
+
+class Linkedlist:
+    def __init__(self, head = None):
+        self.head = head
+
+    def append(self, new_node):
+        current = self.head
+        if current:
+            while current.next:
+                current = current.next
+                current.next = new_node
+        else: 
+            self.head = new_node
+
+
+
+
